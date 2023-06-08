f8f3f52b0c77baa12896eea40e4c1fd2ff6aaa2d
Arfaoui Hania <haniaarfaoui@gmail.com>
2023-06-08 01:27:17 +0100

    newfinal

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.demo.controllers;
 
+import com.microsoft.applicationinsights.TelemetryConfiguration;
+import com.microsoft.applicationinsights.TelemetryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 @Controller
 public class HelloController {
+    private TelemetryClient telemetryClient;
 
+    public HelloController() {
+        String instrumentationKey ="a14ba32c-32dd-4b6f-9372-ab97d448caaf" ;
+        TelemetryConfiguration configuration = TelemetryConfiguration.createDefault();
+        configuration.setInstrumentationKey(instrumentationKey);
+        telemetryClient = new TelemetryClient(configuration);
+    }
 
     @GetMapping("/hello")
     public String showForm(Model model) {
@@ -19,6 +27,10 @@ public class HelloController {
     @PostMapping("/hello")
     public String sayHello(Model model) {
         model.addAttribute("message", "Hello, you!");
+
+        // Track telemetry event
+        telemetryClient.trackEvent("HelloButtonClicked");
+
         return "hello";
     }
 }
